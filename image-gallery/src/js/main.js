c4bd420ab4bfccbb13ba2feec7b52bca4c560877
LinderJK <slepchenkovia@gmail.com>
2024-09-27 00:03:09 +0300

    feat: add select for color, submit function refactor: move fetchImages fnc to api

--- image-gallery/src/js/main.js
+++ image-gallery/src/js/main.js
@@ -1,29 +1,43 @@
+import githubLogo from "../assets/github-mark-white.svg";
+import rssLogo from "../assets/rss-logo.svg";
 import {renderContent} from "./render.js";
-import fetchData from "./services/API.js";
-import githubLogo from '../assets/github-mark-white.svg';
-import rssLogo from '../assets/rss-logo.svg';
+import {fetchImages} from "./services/API.js";
+
 
 const PAGE = 1;
-const PER_PAGE = 10;
+const PER_PAGE = 9;
 const QUERY = "cat";
 const COLOR = "green";
 const data = new Date().getFullYear();
+let currentColor = '';
 
 const app = document.querySelector('#app');
 app.innerHTML = `
  <div class="container mx-auto p-4">
     <header class="py-12">
       <h1 class="relative text-5xl uppercase text-white z-10">Image Gallery</h1>
-      <div class="flex gap-4 my-2">
-
-      <input id="search-input" class="w-full rounded-2xl p-3 shadow-2xl hover:shadow-lime-300 duration-700 ease-in-out focus:outline-none focus:ring-0 focus:border-lime-600 focus:shadow-lime-300" placeholder="Inter your search"  autofocus autocomplete="off" />
-      <input type="color" id="color-input" class="w-20" datatype="hex">
-      <button id="search-btn" class="min-w-44 rounded-2xl p-3 shadow-2xl text-white bg-lime-700 hover:bg-lime-400 hover:shadow-inner hover:scale-105 duration-700 ease-in-out uppercase font-medium">
-  Search
-</button>
-
-
-      </div>
+      <form class="flex gap-4 my-2" id="search-form">
+        <input required name="search-query" type="text" id="search-input" class="w-full rounded-2xl p-3 shadow-2xl hover:shadow-lime-300 duration-700 ease-in-out focus:outline-none focus:ring-0 focus:border-lime-600 focus:shadow-lime-300" placeholder="Inter your search"  autofocus autocomplete="off" />
+        <div class="flex items-center">
+          <select id="color-select" class="ml-1 bg-inherit border border-lime-700 text-white rounded-lg p-2 cursor-pointer">
+              <option value="" selected>Chooose color?</option>
+              <option value="black_and_white">Black and white</option>
+              <option value="black">Black</option>
+              <option value="white">White</option>
+              <option value="yellow">Yellow</option>
+              <option value="orange">Orange</option>
+              <option value="red">Red</option>
+              <option value="purple">Purple</option>
+              <option value="magenta">Magenta</option>
+              <option value="green">Green</option>
+              <option value="teal">Teal</option>
+              <option value="blue">Blue</option>
+          </select>
+        </div>
+        <button type="submit" id="search-btn" class="min-w-44 rounded-2xl p-3 shadow-2xl text-white bg-lime-700 hover:bg-lime-400 hover:shadow-inner hover:scale-105 duration-700 ease-in-out uppercase font-medium">
+          Search
+        </button>
+      </form>
     </header>
     <main id="images-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
@@ -41,38 +55,37 @@ app.innerHTML = `
   </div>
 `;
 
+const colorSelect = document.querySelector('#color-select');
 const imagesContainer = document.querySelector('#images-container');
+const form = document.querySelector('#search-form');
 
-
-addEventListener('click', (event) => {
-  if (event.target.id === 'search-input') {
-    const qery = event.target.value;
-    console.log(qery)
+colorSelect.addEventListener('change', (event) => {
+  if (event.target.value === '') {
+    currentColor = '';
   }
+  currentColor = event.target.value;
+  console.log(currentColor);
+})
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const query = formData.get('search-query');
+    const data = await fetchImages(query, PAGE, PER_PAGE, currentColor);
+    if(data && imagesContainer) {
+      renderContent(data.results, imagesContainer);
+      console.log(data);
+    }
 })
 
 async function startApp() {
   console.log('app started');
-  const images = await fetchImages();
+  const images = await fetchImages(QUERY, PAGE, PER_PAGE, COLOR);
   if(images && imagesContainer) {
     renderContent(images.results, imagesContainer);
     console.log(images);
   }
 }
-
-async function fetchImages(query = QUERY, page = PAGE, perPage = PER_PAGE, color = COLOR) {
-  try {
-    const images = await fetchData(query, page, perPage, color);
-    console.log(images);
-    if (images) {
-      console.log(images);
-      return images;
-    }
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-}
-
 
 startApp();
