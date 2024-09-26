06bc85664bf8ec956dc489306b2232198addd458
LinderJK <slepchenkovia@gmail.com>
2024-09-26 21:35:08 +0300

    feat: add footer

--- image-gallery/src/js/main.js
+++ image-gallery/src/js/main.js
@@ -1,24 +1,41 @@
-import fetchData from "./services/API";
-import renderImages from "./renderImages";
+import {renderContent} from "./render.js";
+import fetchData from "./services/API.js";
+import githubLogo from '../assets/github-mark-white.svg';
+import rssLogo from '../assets/rss-logo.svg';
 
 const PAGE = 1;
 const PER_PAGE = 10;
-const QUERY = "cats";
-const COLOR = "orange";
+const QUERY = "cat";
+const COLOR = "green";
+
+
+const data = new Date().getFullYear();
 
 const app = document.querySelector('#app');
 app.innerHTML = `
  <div class="container mx-auto p-4">
-    <div class="p-4 ">
-      <h1 class="relative text-5xl uppercase text-white z-10">123</h1>
+    <header class="py-12">
+      <h1 class="relative text-5xl uppercase text-white z-10">Image Gallery</h1>
       <div class="flex gap-4 my-2">
+
       <input id="search-input" class="w-full rounded-2xl p-3 shadow-2xl hover:shadow-lime-300 duration-700 ease-in-out focus:outline-none focus:ring-0 focus:border-lime-600 focus:shadow-lime-300" placeholder="Inter your search"  autofocus autocomplete="off" />
-      <button id="search-btn" class="min-w-44 rounded-2xl p-3 shadow-2xl bg-lime-300 hover:bg-lime-700 duration-700 ease-in-out uppercase font-medium">Search</button>
+      <input type="color" id="color-input" class="w-20" datatype="hex">
+      <button id="search-btn" class="min-w-44 rounded-2xl p-3 shadow-2xl text-white bg-lime-700 hover:bg-lime-400 duration-700 ease-in-out uppercase font-medium">Search</button>
       </div>
-    </div>
-    <div id="images-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    
-    </div>
+    </header>
+    <main id="images-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+    </main>
+    <footer class="text-white py-4 flex gap-4 justify-between items-center">
+    <a href="https://github.com/LinderJK" target="_blank" class="hover:rotate-180 transition-transform duration-300 ease-in-out">
+      <img src="${githubLogo}" alt="logo-github" class="w-10">
+      </a>
+      <p>Created by LinderJK</p>
+      <p>&copy; ${data}</p>
+      <a href="https://rs.school/courses/javascript-preschool-ru" target="_blank" class="hover:rotate-180 transition-transform duration-300 ease-in-out">
+       <img src="${rssLogo}" alt="logo-rss" class="w-10">
+      </a>
+    </footer>
   </div>
 `;
 
@@ -36,7 +53,7 @@ async function startApp() {
   console.log('app started');
   const images = await fetchImages();
   if(images && imagesContainer) {
-    renderImages(images.results, imagesContainer);
+    renderContent(images.results, imagesContainer);
     console.log(images);
   }
 }
