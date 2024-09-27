2dc263e3cf6d14fe086ea0debd0c1c12df9633bb
LinderJK <slepchenkovia@gmail.com>
2024-09-27 15:18:42 +0000

    refactor: rename infiniteScrooll move to utils

---
+++ image-gallery/src/js/utils/infiniteScroll.js
@@ -0,0 +1,56 @@
+import { getFromLS } from "../services/LS";
+import { fetchImages } from "../services/API";
+import { applyInfoBlock, applyNewContent } from "../render";
+
+export function infiniteScroll(container) {
+  let canScroll = true;
+  let isLoading = false;
+  let resizeTimeout;
+  let count = 1;
+  const treshhold = 200;
+
+  window.onscroll = () => {
+    if(!canScroll || isLoading) return;
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - treshhold) {
+      isLoading = true;
+      canScroll = false;
+      count += 1;
+      infiniteScrollCreateData(count, container)
+      .then(() => {
+        isLoading = false;
+        canScroll = true;
+      })
+      .catch((error) => {
+        console.log(error);
+        isLoading = false;
+        canScroll = true;
+      })
+      console.log('bottom');
+    }
+  }
+
+  window.addEventListener('resize', (event) => {
+    console.log(event);
+    canScroll = false;
+    console.log('canCalcScroll', canScroll);
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+      canScroll = true;
+      console.log('canCalcScroll', canScroll);
+    }, 1000);
+  })
+}
+
+
+async function infiniteScrollCreateData(page, container) {
+  const queryData = getFromLS();
+  const data = await fetchImages(queryData.query, page, 9, queryData.color);
+  if (data.total_pages === page) {
+    applyInfoBlock(container, `No more images found for your search :(`);
+    return;
+  }
+  if(data) {
+    applyNewContent(data.results, container, true);
+    console.log('data', data);
+  }
+}
